00f81db6d4ed4f4c59bc074e057a401e87aa335c
panda-jiny <kgavuru@gmail.com>
2026-08-15 18:14:28 +0900

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,18 +1,6 @@
 let movieBoard = document.querySelector("#movieBoard");
 let apikey = "bb6e4b0c3a0d75468f7b7a5c1bca2b61";
 
-// swiper card eff
-var swiper = new Swiper(".mySwiper", {
-  effect: "cards",
-  grabCursor: true,
-  passiveListners: false,
-  simulateTouch: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
 //서버에서 원하는 영화 가지고 오기
 movie = async (lists) => {
   //   console.log("movie");
@@ -29,11 +17,6 @@ movie = async (lists) => {
   movieList = data.results;
   console.log(movieList);
   render(movieList);
-
-  // new Swiper(".mySwiper", {
-  //   effect: "cards",
-  //   grabCursor: true,
-  // });
 };
 
 //화면에 나타내는 함수
@@ -44,35 +27,18 @@ render = (movieList) => {
     // console.log(movie.poster_path);
 
     card = `
-        <div class="card swiper-slide">
+        <div class="card">
           <div class="imgBox">
-          <div class="backLight"></div> 
-              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
-              <p class="avg"><span>평점 </span>${Math.round(movie.vote_average)}</p>
-              <h3>${movie.title}</h3>
-          </div>
-           
+            <div class="backLight"></div> 
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+            <p class="avg"><span>평점 </span>${Math.round(movie.vote_average)}</p>
+            <h3>${movie.title}</h3>
+          </div>           
         </div>
     `;
 
     movieBoard.innerHTML += card;
   });
-  var swiper = new Swiper(".mySwiper", {
-    effect: "cards",
-    grabCursor: true,
-    passiveListners: false,
-    simulateTouch: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
-  // console.log("width:", swiper.width);
-  // console.log("slides:", swiper.slides.length);
-  // console.log("isBeginning:", swiper.isBeginning);
-  // console.log("isEnd:", swiper.isEnd);
-  // console.log("allowTouchMove:", swiper.allowTouchMove);
 };
 
 function over(text, limit) {
